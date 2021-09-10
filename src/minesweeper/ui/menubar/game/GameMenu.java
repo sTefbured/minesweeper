856211878f0c8bda4cd4883f2445dc8be9defe78
sTefbured <stef.for.ever2000@gmail.com>
2021-09-10 12:19:39 +0300

    Close #2. Add Game menu.

---
+++ src/minesweeper/ui/menubar/game/GameMenu.java
@@ -0,0 +1,22 @@
+package minesweeper.ui.menubar.game;
+
+import javax.swing.*;
+
+public class GameMenu extends JMenu {
+    public GameMenu() {
+        super("Game");
+        JMenuItem newGameItem = new JMenuItem("New game");
+        JMenuItem restartItem = new JMenuItem("Restart");
+        JMenuItem exitItem = new JMenuItem("Exit");
+        newGameItem.addActionListener((e) -> {
+
+        });
+        restartItem.addActionListener((e) -> {
+
+        });
+        exitItem.addActionListener((e) -> System.exit(0));
+        add(newGameItem);
+        add(restartItem);
+        add(exitItem);
+    }
+}
